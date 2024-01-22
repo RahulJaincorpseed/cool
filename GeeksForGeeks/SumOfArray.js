db451f7de6c83feec706702c71ce6f88debf56c9
RahulJaincorpseed <rahul.jain@corpseed.com>
2024-01-22 15:35:09 +0530

    add Array of Sum Program in GFG

---
+++ GeeksForGeeks/SumOfArray.js
@@ -0,0 +1,14 @@
+
+// Program of Sum Of Array
+
+class Solution {
+    sum(arr,n) {
+        // code here
+        let sum = 0;
+        for(let i=0;i<n;i++){
+            sum += arr[i];
+        }
+        
+        return sum;
+    }
+}
