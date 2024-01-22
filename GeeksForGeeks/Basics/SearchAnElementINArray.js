2a7e0c22110a3190295405f74ef9a201ff8aae1b
RahulJaincorpseed <rahul.jain@corpseed.com>
2024-01-22 15:53:04 +0530

    Search an Element in an array program

---
+++ GeeksForGeeks/Basics/SearchAnElementINArray.js
@@ -0,0 +1,13 @@
+// Program of Search an Element in an Array
+
+class Solution {
+    search(arr, N, X) {
+        // write your code here
+        for(let i=0; i<N;i++){
+            if(arr[i] === X){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
